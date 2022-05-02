54a1b9f73716a09a231d86ff026a61f8e5eb5e57
soner.acar <soner.acar@vbt.com.tr>
2022-05-03 01:50:59 +0300

    Some files are change namespace.

--- SNR.Common/Validation/IValidationHelper.cs
+++ SNR.Common/Validation/IValidationHelper.cs
@@ -1,4 +1,4 @@
-﻿namespace Common
+﻿namespace SNR.Common
 {
     public interface IValidationHelper
     {
--- SNR.Common/Validation/ValidationHelper.cs
+++ SNR.Common/Validation/ValidationHelper.cs
@@ -1,31 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Common
+namespace SNR.Common;
+
+public class ValidationHelper : IValidationHelper
 {
-    public class ValidationHelper : IValidationHelper
+    public void Get(int id)
     {
-        public void Get(int id)
+        if (id <= 0)
         {
-            if (id <= 0)
-            {
-                throw new NotificationException("id parametresi boş olamaz");
-            }
+            throw new NotificationException("id parametresi boş olamaz");
+        }
+    }
+
+    public void Save<TModel>(TModel request)
+    {
+        if (request == null)
+        {
+            throw new NotificationException("request parametresi boş olamaz");
         }
 
-        public void Save<TModel>(TModel request)
+        var context = new ValidationContext(request, null, null);
+        var validationResults = new List<ValidationResult>();
+        bool valid = Validator.TryValidateObject(request, context, validationResults, true);
+        if (!valid)
         {
-            if (request == null)
-            {
-                throw new NotificationException("request parametresi boş olamaz");
-            }
-
-            var context = new ValidationContext(request, null, null);
-            var validationResults = new List<ValidationResult>();
-            bool valid = Validator.TryValidateObject(request, context, validationResults, true);
-            if (!valid)
-            {
-                throw new NotificationException(JsonConvert.SerializeObject(validationResults.Select(k => k.ErrorMessage).ToList()));
-            }
+            throw new NotificationException(JsonConvert.SerializeObject(validationResults.Select(k => k.ErrorMessage).ToList()));
         }
     }
 }
